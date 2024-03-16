1bde2beb50867cc388f2ae91a7d4313252e26f88
Aditya Agrawal <agarwaladitya0106@gmail.com>
2024-03-16 11:16:33 +0530

    Delete selenium/tempCodeRunnerFile.py

--- selenium/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-
-s=service("C:/Users/agarw/Desktop/chromedriver-win64/chromedriver-win64/chromedriver.exe")
-
-driver=webdriver.Chrome(service=s)
-
-driver.get('https://www.smartprix.com/mobiles')
